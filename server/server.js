0c632ea20c9e8a1853d6918c551ad0f119b0c233
Jason Chen <jason.sl.chen@gmail.com>
2019-12-17 16:35:51 -0800

    set up email submission form frontend to talk to backend

--- server/server.js
+++ server/server.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const path = require('path');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 const app = express();
 const port = 3000;
 
-app.use(morgan());
+app.use(bodyParser.json());
+app.use(morgan('tiny'));
 app.use('/', express.static(path.resolve(__dirname, '../public')));
+
+app.post('/api/contactForm', (req, res) => {
+  console.log(req.body);
+  res.send(200);
+});
 
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/index.html'), (err) => {
@@ -15,4 +22,5 @@ app.get('/*', (req, res) => {
     }
   });
 });
+
 app.listen(port, () => { console.log(`we be listening on port ${port}`); });
--- src/contact.jsx
+++ src/contact.jsx
@@ -1,12 +1,88 @@
 import React from 'react';
+import axios from 'axios';
 
-const Contact = (props) => {
-  console.log(props);
-  return (
-    <div>
-      <div>Contact Me</div>
-    </div>
-  );
-};
+class Contact extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      email: '',
+      subject: '',
+      message: '',
+    };
+    this.inputData = this.inputData.bind(this);
+    this.sendEmail = this.sendEmail.bind(this);
+  }
+
+  inputData(event) {
+    this.setState({
+      [event.target.id]: event.target.value,
+    });
+  }
+
+  sendEmail(event) {
+    const {
+      name, email, subject, message,
+    } = this.state;
+    event.preventDefault();
+
+    axios.post('/api/contactForm', {
+      name,
+      email,
+      subject,
+      message,
+    })
+      .then(() => {
+        console.log('hi');
+        this.setState({
+          name: '',
+          email: '',
+          subject: '',
+          message: '',
+        });
+      })
+      .catch((err) => {
+        console.log('error with sending message', err);
+      });
+  }
+
+  render() {
+    const {
+      name, email, subject, message,
+    } = this.state;
+    return (
+      <div>
+        <div>Contact Me</div>
+        <form onSubmit={this.sendEmail}>
+          <div>
+            <label>
+            Name:
+              <input onChange={this.inputData} value={name} id="name" type="text" />
+            </label>
+          </div>
+          <div>
+            <label>
+            Email:
+              <input onChange={this.inputData} value={email} id="email" type="email" />
+            </label>
+          </div>
+          <div>
+            <label>
+            Subject:
+              <input onChange={this.inputData} value={subject} id="subject" type="text" />
+            </label>
+          </div>
+          <div>
+            <label>
+            Message:
+              <textarea onChange={this.inputData} value={message} id="message" />
+            </label>
+          </div>
+          <button type="submit">Send Mail</button>
+        </form>
+      </div>
+    );
+  }
+}
 
 export default Contact;
