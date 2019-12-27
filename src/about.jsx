d1704134cb1f67a2f9e2959a87936dd1803098f4
Jason Chen <jason.sl.chen@gmail.com>
2019-12-27 13:41:06 -0800

    added in resume and thesis pdf and descriptions of those about categories

--- src/about.jsx
+++ src/about.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import e from 'express';
 import styles from './css/about.css';
 
 const About = (props) => {
   const Education = [{
+    school: 'University of Washington',
     degree: 'Masters of Science',
     field: 'Chemistry',
     focus: 'Organic Chemistry',
@@ -13,6 +13,7 @@ const About = (props) => {
     thesisLink: 'http://hdl.handle.net/1773/44113',
   },
   {
+    school: 'University of Southern California',
     degree: 'Bachelors of Science',
     field: 'Chemistry',
     focus: 'Chemical Biology',
@@ -20,30 +21,62 @@ const About = (props) => {
     end: 'June, 2019',
   },
   {
+    school: 'Homestead High School',
     degree: 'High School Diploma',
     start: 'August 2009',
     end: 'June, 2013',
   },
   ];
+  const Tech = [{
+    category: 'Front End',
+    list: 'Javascript, ES6, React, HTML5, CSS, Redux, Styled-Components, CSS Modules, Webpack, Babel',
+  },
+  {
+    category: 'Back End',
+    list: 'Node.js, Express, Sequelize, Mongoose',
+  },
+  {
+    category: 'Databases',
+    list: ' Apache Cassandra, MySQL, MongoDB, PostgreSQL',
+  },
+  {
+    category: 'Testing',
+    list: 'Jest, Enzyme, Mocha, Chai',
+  },
+  {
+    category: 'Deployment',
+    list: 'Docker, AWS EC2/S3/CloudFront, Grunt',
+  }];
   return (
     <div className={styles.about}>
       <div>
         <img className={styles.bio_image} alt="bio-portrait" src="./Jason-Chen.jpg" />
+        <div>
+          <a href="/Resume.pdf" target="_blank">Resume</a>
+        </div>
       </div>
       <div>
-        <div>
+        <div className={styles.about_categories}>
           <div>
           Biography:
           </div>
         </div>
-        <div>Background:</div>
-        <div>
+        <div className={styles.about_categories}>
+          <div>Background:</div>
+
+        </div>
+        <div className={styles.about_categories}>
           <div>Education:</div>
-          <div>
-            {/* {Education.map((ed) => (
-              <div>
+          <div className={styles.about_category_inner}>
+            {Education.map((ed) => (
+              <div className={styles.about_degrees}>
+                <div className={styles.about_school}>
+                  {ed.school}
+                  <span>
+                    {`${ed.start} - ${ed.end}`}
+                  </span>
+                </div>
                 <div>
-Degree:
                   {ed.degree}
                 </div>
                 {ed.field ? (
@@ -53,7 +86,7 @@ Degree:
                       {ed.field}
                     </div>
                     <div>
-Focus:
+Emphasis:
                       {ed.focus}
                     </div>
                     {ed.thesis ? (
@@ -62,26 +95,40 @@ Focus:
 Thesis:
                           {ed.thesis}
                         </div>
-                        <div>{ed.thesisLink}</div>
-                        <a href="./Mastersthesis.pdf">Thesis</a>
+                        <div>
+Online:
+                          <a target="_blank" href={ed.thesisLink}>Thesis URL</a>
+                        </div>
+                        PDF:
+                        <a target="_blank" href="./Mastersthesis.pdf">Thesis PDF</a>
                       </div>
                     ) : ''}
                   </div>
                 ) : ''}
-                <div>
-Start:
-                  {ed.start}
-                </div>
-                <div>
-End:
-                  {ed.end}
-                </div>
+
               </div>
-            ))} */}
+            ))}
           </div>
         </div>
-        <div>Experience: </div>
-        <div>Technologies: </div>
+        <div className={styles.about_categories}>
+          <div>
+          Experience:
+          </div>
+        </div>
+        <div className={styles.about_categories}>
+          <div>Technologies:</div>
+          <div className={styles.about_category_inner}>
+            {Tech.map((eachTech) => (
+              <div className={styles.about_tech}>
+                <div>
+                  {eachTech.category}
+:
+                </div>
+                <div>{eachTech.list}</div>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   );
