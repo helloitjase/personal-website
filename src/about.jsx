2716686aff355e2564cd115a3e2a18b2d631a550
Jason Chen <jason.sl.chen@gmail.com>
2019-12-27 09:51:18 -0800

    added in education inforamtion

--- src/about.jsx
+++ src/about.jsx
@@ -1,19 +1,89 @@
 import React from 'react';
+import e from 'express';
 import styles from './css/about.css';
 
-const About = (props) => (
-  <div className={styles.about}>
-    <div>
-      <img className={styles.bio_image} alt="bio-portrait" src="./Jason-Chen.jpg" />
+const About = (props) => {
+  const Education = [{
+    degree: 'Masters of Science',
+    field: 'Chemistry',
+    focus: 'Organic Chemistry',
+    start: 'September, 2017',
+    end: 'June, 2019',
+    thesis: 'Hydroalkylation of alkynes via dual copper-nickel catalysis',
+    thesisLink: 'http://hdl.handle.net/1773/44113',
+  },
+  {
+    degree: 'Bachelors of Science',
+    field: 'Chemistry',
+    focus: 'Chemical Biology',
+    start: 'August, 2013',
+    end: 'June, 2019',
+  },
+  {
+    degree: 'High School Diploma',
+    start: 'August 2009',
+    end: 'June, 2013',
+  },
+  ];
+  return (
+    <div className={styles.about}>
+      <div>
+        <img className={styles.bio_image} alt="bio-portrait" src="./Jason-Chen.jpg" />
+      </div>
+      <div>
+        <div>
+          <div>
+          Biography:
+          </div>
+        </div>
+        <div>Background:</div>
+        <div>
+          <div>Education:</div>
+          <div>
+            {/* {Education.map((ed) => (
+              <div>
+                <div>
+Degree:
+                  {ed.degree}
+                </div>
+                {ed.field ? (
+                  <div>
+                    <div>
+                   Field:
+                      {ed.field}
+                    </div>
+                    <div>
+Focus:
+                      {ed.focus}
+                    </div>
+                    {ed.thesis ? (
+                      <div>
+                        <div>
+Thesis:
+                          {ed.thesis}
+                        </div>
+                        <div>{ed.thesisLink}</div>
+                        <a href="./Mastersthesis.pdf">Thesis</a>
+                      </div>
+                    ) : ''}
+                  </div>
+                ) : ''}
+                <div>
+Start:
+                  {ed.start}
+                </div>
+                <div>
+End:
+                  {ed.end}
+                </div>
+              </div>
+            ))} */}
+          </div>
+        </div>
+        <div>Experience: </div>
+        <div>Technologies: </div>
+      </div>
     </div>
-    <div>
-Biography:
-      <div>Background:</div>
-      <div>Education: </div>
-      <div>Experience: </div>
-      <div>Technologies: </div>
-    </div>
-  </div>
-);
-
+  );
+};
 export default About;
